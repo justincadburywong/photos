a3275ec0563656c02526b99ce3ea39c546e3ff38
Justin Wong <justincadburywong@gmail.com>
2021-10-27 16:55:26 -0700

    albums/photos mvp with friendly_id

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :albums
   devise_for :users
+  root 'albums#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
